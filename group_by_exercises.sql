b83b04bfca1eaad8af4c555c8300a8c92d2a134b
Andrew Lange <andrew.lange13@gmail.com>
2022-03-17 16:50:14 -0500

    completes group by exercises

---
+++ group_by_exercises.sql
@@ -0,0 +1,23 @@
+USE employees;
+
+SELECT DISTINCT title FROM titles;
+
+SELECT CONCAT(first_name, ' ', last_name)
+FROM employees
+WHERE last_name LIKE 'E%'
+   AND last_name LIKE '%E'
+GROUP BY first_name, last_name
+ORDER BY last_name;
+
+SELECT COUNT(last_name), last_name
+FROM employees
+WHERE last_name LIKE '%q%'
+  AND last_name NOT LIKE '%qu%'
+GROUP BY last_name;
+
+SELECT COUNT(*), gender
+FROM employees
+WHERE first_name = 'Irena'
+    OR first_name = 'Vidya'
+    OR first_name = 'Maya'
+GROUP BY gender;
