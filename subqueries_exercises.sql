a39f56f9f87dfe3a5f73e408f864e2f7504bee92
Andrew Lange <andrew.lange13@gmail.com>
2022-03-18 15:42:23 -0500

    completes subqueries exercise without bonuses

--- subqueries_exercises.sql
+++ subqueries_exercises.sql
@@ -16,4 +16,13 @@ WHERE titles.emp_no IN (
     WHERE employees.first_name = 'Aamod'
           );
 
+SELECT employees.first_name AS 'First Name', employees.last_name AS 'Last Name', employees.gender AS 'Gender'
+FROM employees
+WHERE employees.emp_no IN (
+    SELECT dept_manager.emp_no
+    FROM dept_manager
+    WHERE employees.gender = 'F'
+    AND dept_manager.to_date = '9999-01-01'
+    )
+
 
