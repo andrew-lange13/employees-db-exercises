ffe6d465cf417496c0747765b55b99870710e789
Andrew Lange <andrew.lange13@gmail.com>
2022-03-18 14:31:36 -0500

    completes join exercises without bonus

--- join_exercises.sql
+++ join_exercises.sql
@@ -22,3 +22,12 @@ WHERE dept_emp.dept_no = 'd009'
 AND titles.to_date = '9999-01-01'
 AND dept_emp.to_date = '9999-01-01'
 GROUP BY titles.title;
+
+SELECT departments.dept_name AS 'Department Name', CONCAT(employees.first_name, ' ', employees.last_name) AS 'Department Manager', salaries.salary AS 'Salary'
+FROM employees
+JOIN dept_manager ON employees.emp_no = dept_manager.emp_no
+JOIN departments ON dept_manager.dept_no = departments.dept_no
+JOIN salaries ON employees.emp_no = salaries.emp_no
+WHERE dept_manager.to_date = '9999-01-01'
+AND salaries.to_date = '9999-01-01'
+ORDER BY departments.dept_name;
