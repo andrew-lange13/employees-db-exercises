d3938fd4786aee4c7c2f741962c2ca5619abba2b
Andrew Lange <andrew.lange13@gmail.com>
2022-03-18 14:09:10 -0500

    lists current title of customer service employees

--- join_exercises.sql
+++ join_exercises.sql
@@ -13,3 +13,12 @@ FROM employees
          JOIN departments ON dept_manager.dept_no = departments.dept_no
 WHERE dept_manager.to_date = '9999-01-01' AND employees.gender = 'F'
 ORDER BY dept_name;
+
+SELECT titles.title AS 'title', COUNT(titles.emp_no) AS 'Total'
+FROM titles
+JOIN dept_emp ON titles.emp_no = dept_emp.emp_no
+JOIN departments ON dept_emp.dept_no = departments.dept_no
+WHERE dept_emp.dept_no = 'd009'
+AND titles.to_date = '9999-01-01'
+AND dept_emp.to_date = '9999-01-01'
+GROUP BY titles.title;
