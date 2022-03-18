85c068f0dba083dc50d229f9ff7ac7e0b89f8d54
Andrew Lange <andrew.lange13@gmail.com>
2022-03-18 15:58:31 -0500

    completes subqueries exercise with bonuses

--- subqueries_exercises.sql
+++ subqueries_exercises.sql
@@ -14,7 +14,7 @@ WHERE titles.emp_no IN (
     SELECT emp_no
     FROM employees
     WHERE employees.first_name = 'Aamod'
-          );
+);
 
 SELECT employees.first_name AS 'First Name', employees.last_name AS 'Last Name', employees.gender AS 'Gender'
 FROM employees
@@ -22,7 +22,30 @@ WHERE employees.emp_no IN (
     SELECT dept_manager.emp_no
     FROM dept_manager
     WHERE employees.gender = 'F'
-    AND dept_manager.to_date = '9999-01-01'
+      AND dept_manager.to_date = '9999-01-01'
+);
+
+
+SELECT departments.dept_name AS 'Departments with Female Managers'
+FROM departments
+WHERE departments.dept_no IN (
+    SELECT dept_manager.dept_no
+    FROM dept_manager
+    WHERE dept_manager.emp_no IN (
+        SELECT employees.emp_no
+        FROM employees
+        WHERE employees.gender = 'F'
     )
+      AND dept_manager.to_date = '9999-01-01'
+);
 
-
+SELECT CONCAT(employees.first_name, ' ', employees.last_name) AS 'Employee with Highest Salary'
+FROM employees
+WHERE employees.emp_no IN (
+    SELECT salaries.emp_no
+    FROM salaries
+    WHERE salary IN (
+        SELECT MAX(salary)
+        FROM salaries
+    )
+);
